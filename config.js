3ea923c92464669a585193ed91626af47c098e33
John Tomaselli <jrt@gtz.com>
2015-11-02 17:03:54 -0500

    first commit for an:v1,0,6

---
+++ config.js
@@ -0,0 +1,8 @@
+// module.exports = {
+//     "API_LOGIN_ID": "2msN9nrBG8K",
+//     "TRANSACTION_KEY": "43jNykM6kC8v87Nb"
+// };
+module.exports = {
+    "API_LOGIN_ID": "8mHN4Tk9K",
+    "TRANSACTION_KEY": "3U3wN24Hqm7C5V98"
+};
